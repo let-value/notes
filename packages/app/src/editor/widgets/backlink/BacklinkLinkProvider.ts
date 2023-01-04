8f52cf7002776eb1a91c68f0bba87f7dafe93810
let-value <let.value@gmail.com>
2023-01-04 16:20:13 +0600

    some refatoring

--- packages/app/src/editor/widgets/backlink/BacklinkLinkProvider.ts
+++ packages/app/src/editor/widgets/backlink/BacklinkLinkProvider.ts
@@ -1,12 +1,14 @@
+import { container } from "@/container";
 import { CancellationToken, editor, languages } from "monaco-editor";
 
+const tokensService = container.get("tokensService");
+
 export class BacklinkLinkProvider implements languages.LinkProvider {
-    provideLinks(model: editor.ITextModel, token: CancellationToken): languages.ProviderResult<languages.ILinksList> {
+    provideLinks(model: editor.ITextModel): languages.ProviderResult<languages.ILinksList> {
+        const tokens = tokensService.getModelTokens(model);
         throw new Error("Method not implemented.");
     }
     resolveLink?:
         | ((link: languages.ILink, token: CancellationToken) => languages.ProviderResult<languages.ILink>)
         | undefined;
 }
-
-languages.registerLinkProvider("markdown", new BacklinkLinkProvider());
