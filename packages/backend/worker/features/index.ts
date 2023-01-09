8ad9c53d94bbe64af4c3de6a6a119d3e4e8315f9
let-value <let.value@gmail.com>
2023-01-10 01:14:38 +0600

    remove example

--- packages/backend/worker/features/index.ts
+++ packages/backend/worker/features/index.ts
@@ -1,12 +1,3 @@
 export * from "./leader";
 export * from "./workpaces";
 export * from "./workspace";
-
-import { rc, ready } from "rclone";
-
-await ready;
-try {
-    console.log("core/version", rc("core/version", null));
-} catch (e) {
-    console.error(e);
-}
