c2195d4ea8eac5b4c090ebcf43111a7f3d65a812
let-value <let.value@gmail.com>
2023-02-06 19:20:45 +0600

    display table view

---
+++ packages/app/src/components/Editors/Database/TableView.tsx
@@ -0,0 +1,56 @@
+import { databaseMetaState } from "@/atom/database/databaseMetaState";
+import { Intent } from "@blueprintjs/core";
+import { Column, EditableCell2, Table2 } from "@blueprintjs/table";
+import { FC, useCallback } from "react";
+import { useRecoilValue } from "recoil";
+import { databaseViewState } from "../../../atom/database/databaseViewState";
+import type { ViewProps } from "./View";
+
+export const TableView: FC<ViewProps> = ({ workspace, item, view }) => {
+    const meta = useRecoilValue(databaseMetaState({ workspaceId: workspace.id, path: item.path }));
+    const data = useRecoilValue(databaseViewState({ workspaceId: workspace.id, path: item.path, view: view.name }));
+
+    const columns = Math.max(...(data ?? []).map((row) => row.length));
+
+    const handleRenderCell = useCallback(
+        (columnIndex: number, rowIndex: number) => {
+            const cell = data?.[rowIndex]?.[columnIndex];
+
+            const error = typeof cell === "object";
+            const value = error ? cell?.value : cell;
+
+            return <EditableCell2 value={value?.toString() ?? ""} intent={error ? Intent.DANGER : undefined} />;
+        },
+        [data],
+    );
+
+    if (!data) {
+        return null;
+    }
+
+    return (
+        <Table2 numRows={data?.length} enableRowResizing={false}>
+            {Array.from({ length: columns }).map((_, i) => (
+                <Column
+                    key={i}
+                    name={columnNumberToExcelColumnName(i + 1)}
+                    cellRenderer={handleRenderCell.bind(undefined, i)}
+                />
+            ))}
+        </Table2>
+    );
+};
+
+export function columnNumberToExcelColumnName(columnNumber: number) {
+    let dividend = columnNumber;
+    let columnName = "";
+    let modulo;
+
+    while (dividend > 0) {
+        modulo = (dividend - 1) % 26;
+        columnName = String.fromCharCode(65 + modulo) + columnName;
+        dividend = Math.floor((dividend - modulo) / 26);
+    }
+
+    return columnName;
+}
--- packages/app/src/components/Editors/Database/View.tsx
+++ packages/app/src/components/Editors/Database/View.tsx
@@ -1,22 +1,13 @@
-import { databaseViewState } from "@/atom/database/databaseViewState";
 import { DatabaseMeta, DatabaseView } from "models";
 import { FC } from "react";
-import { useRecoilValue } from "recoil";
 import { EditorProps } from "../EditorProps";
+import { TableView } from "./TableView";
 
 export interface ViewProps extends EditorProps {
     meta: DatabaseMeta;
     view: DatabaseView;
 }
 
-export const View: FC<ViewProps> = ({ workspace, item, view }) => {
-    const data = useRecoilValue(databaseViewState({ workspaceId: workspace.id, path: item.path, view: view.name }));
-
-    return (
-        <>
-            {view.name}
-            <br />
-            {JSON.stringify(data)}
-        </>
-    );
+export const View: FC<ViewProps> = (props) => {
+    return <TableView {...props} />;
 };
